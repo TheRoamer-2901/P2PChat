ca37acfd531a7c2f86166b5be8aa41a58a6e29f5
The Roamer <dangkhoa29012002@gmail.com>
2022-11-24 09:39:38 +0700

    add basic authentication functionality

---
+++ server/controller/onlineUserController.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const User = require('../model/User');
+
+const logIn = async (userName, password, socket) => {
+    const user = await User.findOne({name: userName}).exec();
+    if(!user) console.log("user not exist!");
+    console.log(user);
+    const matchedPassword = await bcrypt.compare(password, user.password);
+    if(matchedPassword) {
+        await User.updateOne({name: userName}, {$set: {online: true}}).exec()
+        socket.emit('loginSuccess', user);
+    }
+    else{
+        console.log("wrong password");
+    }
+}
+
+const logOut = async (userName, socket) => {
+    await User.updateOne({name : userName}, {$set: {online: false}}).exec()
+    const user = await User.findOne({name: userName});
+    console.log(user);
+}
+
+module.exports = {
+    logIn,
+    logOut
+}
